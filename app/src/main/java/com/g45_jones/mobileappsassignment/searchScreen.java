7aa769b0f0677dbc7987cdefce722defaca2d089
greg <gregcj@hotmail.co.uk>
2018-12-21 18:44:58 +0000

    started working on parameterisation
    Sending parameters is easy since they are get requests so can just append the parameters to the URL string. However to work with the response object need to convert it into a jsonobject which is not ideal since there is a JSONobject request in volley which might be more appropriate.

--- app/src/main/java/com/g45_jones/mobileappsassignment/searchScreen.java
+++ app/src/main/java/com/g45_jones/mobileappsassignment/searchScreen.java
@@ -3,6 +3,7 @@ package com.g45_jones.mobileappsassignment;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.system.ErrnoException;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -12,10 +13,15 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class searchScreen extends AppCompatActivity {
@@ -32,7 +38,7 @@ public class searchScreen extends AppCompatActivity {
     }
 
     /*
-    This class fetches the data from the company house api.
+    This function fetches the data from the company house api.
     The user input is taken from the search widget (STRING) and sent to the api.
     Results are displayed below.
     */
@@ -42,7 +48,7 @@ public class searchScreen extends AppCompatActivity {
         final TextView mTextView = (TextView)findViewById(R.id.textView3);
         //Create request queue
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://api.companieshouse.gov.uk/search/companies"; //Search URL
+        String url = "https://api.companieshouse.gov.uk/search/companies?q="+"L"; //Search URL
 
         //Request a response from the URL.
         StringRequest req = new StringRequest(Request.Method.GET, url,
@@ -50,7 +56,16 @@ public class searchScreen extends AppCompatActivity {
                     @Override
                     public void onResponse(String response){
                         //Display the response in the textview
-                        mTextView.setText(response);
+                        try{
+                            /*Convert response into a string - this should be temporary!*/
+                            JSONObject res = new JSONObject(response);
+                            String test = res.getString("kind");
+                            mTextView.setText(test);
+                        }catch(JSONException e)
+                        {
+                            e.printStackTrace();
+                        }
+
                     }
                 }, new Response.ErrorListener(){
             @Override
