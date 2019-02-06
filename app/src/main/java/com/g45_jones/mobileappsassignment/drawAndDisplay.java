45753c2503dc892eaceadc6f12adb09c917fdb2e
greg <gregcj@hotmail.co.uk>
2019-02-06 14:53:05 +0000

    Started drawAndDisplay Activity
    Added an intent and click listener for the results activity which starts the draw activity. Need to pass and retrieve the relevant information.

---
+++ app/src/main/java/com/g45_jones/mobileappsassignment/drawAndDisplay.java
@@ -0,0 +1,13 @@
+package com.g45_jones.mobileappsassignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class drawAndDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_and_display);
+    }
+}
